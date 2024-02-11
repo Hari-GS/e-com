ad8cf19c71248eec7ee12e3af0e1da9fea1d100f
Hari G <sakthihariharan007@gmail.com>
2024-02-11 22:59:54 +0530

    added if condition

--- src/Components/Plans.js
+++ src/Components/Plans.js
@@ -25,33 +25,35 @@ function Plans() {
 
     return (        
             <div className="background-container details-container">
-            {busDetails.map(bus => (
-                <div key={bus.id} className="card">
-                    <div className="bus-details">
-                        <div className="left">
-                            <h3>Bus Number    :</h3>
-                            <p>Number of Days :</p>
-                            <p>Available Seats:</p>
-                            <p>Departure date :</p>
-                            <p>Location       :</p>
+             {loading ? ( // If loading is true, display the spinner
+                <Spinner />
+            ) : (
+                busDetails.map(bus => (
+                    <div key={bus.id} className="card">
+                        <div className="bus-details">
+                            <div className="left">
+                                <h3>Bus Number :</h3>
+                                <p>Number of Days :</p>
+                                <p>Available Seats:</p>
+                                <p>Departure date :</p>
+                                <p>Location :</p>
+                            </div>
+                            <div className="right">
+                                <h3>{bus.busNumber}</h3>
+                                <p>{bus.numberOfDays}</p>
+                                <p>{bus.availableSeats}</p>
+                                <p>{bus.date}</p>
+                                <p>{bus.location}</p>
+                            </div>
                         </div>
-                        <div className="right">
-                            <h3>{bus.busNumber}</h3>
-                            <p>{bus.numberOfDays}</p>
-                            <p>{bus.availableSeats}</p>
-                            <p>{bus.date}</p>
-                            <p>{bus.location}</p>
+                        <div className="b-btn">
+                            <Link to={`/book/${bus.busNumber}`}>
+                                <button>To book</button>
+                            </Link>
                         </div>
                     </div>
-                    <div className="b-btn">
-                    <Link to={`/book/${bus.busNumber}`}>
-                        <button>To book</button>
-                    </Link>
-                    </div>
-                    
-                </div>
-            ))}
-            <Spinner/>
+                ))
+            )}
             </div>
 
     )
